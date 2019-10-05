0839fb987283b0ee0da52dacc94056749821140a
Eric Krawczyk <eric.krawczyk@gmail.com>
2019-10-05 14:19:36 -0400

    Add Page styled component

---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Page = styled.div`
+  height: 11in;
+  width: 8.5in;
+  background-color: white;
+  padding: 0.25in;
+`;
+
+Page.displayName = 'Page';
+
+export default Page;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <h1>Hello World</h1>;
+import Page from './components/Page';
+
+const App = () => (
+  <Page>
+    <h1>Hello World</h1>
+  </Page>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
